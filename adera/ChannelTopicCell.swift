0dc52d96aebacc03b1a14a2d599ee551c8cb297d
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2017-03-18 17:37:20 -0500

    Implement the UI for List of Channels/Topics

---
+++ adera/ChannelTopicCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChannelTopicCell.swift
+//  adera
+//
+//  Created by Svyatoslav Ilinskiy on 3/18/17.
+//  Copyright © 2017 Svyatoslav Ilinskiy. All rights reserved.
+//
+
+import UIKit
+
+class ChannelTopicCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ adera/ChannelTopicTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ChannelTopicTableViewController.swift
+//  adera
+//
+//  Created by Svyatoslav Ilinskiy on 3/18/17.
+//  Copyright © 2017 Svyatoslav Ilinskiy. All rights reserved.
+//
+
+import UIKit
+
+// this is a generalized view controller for list of topics and list of channels.
+// whenever it is created, you should assign it a delegate that should have most of the logic in it.
+class ChannelTopicTableViewController: UITableViewController {
+    var delegate: ChannelTopicTableViewControllerDelegate? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "topicOrTableCell", for: indexPath)
+        if delegate != nil {
+            return delegate!.getCellAt(cell: cell as! ChannelTopicCell, index: indexPath.item)
+        } else {
+            return cell
+        }
+    }
+
+    override func numberOfSections(`in` tableView: UITableView) -> Int {
+        // if delegate is not-null call it. Otherwise return 0.
+        return delegate?.count() ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // if delegate is not-null call it. Otherwise return 0.
+        return delegate?.count() ?? 0
+    }
+}
+
+protocol ChannelTopicTableViewControllerDelegate {
+    func numberOfSections() -> Int
+    func count() -> Int
+    func getCellAt(cell: ChannelTopicCell, index: Int) -> UITableViewCell
+}
