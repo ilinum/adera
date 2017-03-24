8e092ab7ee97ee9dcf783e61e53c04343b9c3480
cindywu2018 <cindywu2018@utexas.edu>
2017-03-23 20:17:53 -0500

    nevermind, brought it back

---
+++ adera/Message.swift
@@ -0,0 +1,27 @@
+//
+// Created by Svyatoslav Ilinskiy on 3/19/17.
+// Copyright (c) 2017 Svyatoslav Ilinskiy. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class Message {
+    var authorUID: String?
+    var content: String?
+
+    init(authorUID: String, content: String) {
+        self.authorUID = authorUID
+        self.content = content
+    }
+
+    init(snapshot: FIRDataSnapshot) {
+        self.authorUID = snapshot.childSnapshot(forPath: "authorUID").value as? String
+        self.content = snapshot.childSnapshot(forPath: "content").value as? String
+    }
+
+    func toDictionary() -> Dictionary<String, Any> {
+        return ["authorUID": authorUID!,
+                "content": content!]
+    }
+}
