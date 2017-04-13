74c50793527b17afb6efea4d1a16a43fbb7e18ac
nathanchapman <nathanchapman@utexas.edu>
2017-04-12 23:17:22 -0500

    Copy channel password to clipboard on click

--- adera/PrivateChannelInfoControllerViewController.swift
+++ adera/PrivateChannelInfoControllerViewController.swift
@@ -9,21 +9,26 @@
 import UIKit
 
 class PrivateChannelInfoViewController: UIViewController {
-    @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var privateChannelLabel: UILabel!
-    
+    @IBOutlet weak var passwordButton: UIButton!
     var channel: Channel? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = channel?.presentableName
-        passwordLabel.text = channel?.password
+        passwordButton.setTitle(channel?.password, for: .normal)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         let color = UIApplication.shared.delegate?.window??.tintColor ?? UIColor.blue
         self.privateChannelLabel.textColor = color
-        self.passwordLabel.textColor = color
         self.view.backgroundColor = UITableView.appearance().backgroundColor
+    }
+    
+    @IBAction func passwordButtonTapped(_ sender: Any) {
+        UIPasteboard.general.string = channel?.password
+        let alertController = UIAlertController(title: "", message: "Channel password copied to clipboard!", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 }
