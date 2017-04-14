6f7499b88feb2a1a95be677d5665fc555ccc4c2f
cindywu2018 <cindywu2018@utexas.edu>
2017-04-14 00:29:36 -0500

    added creationDate to both channels and topics. implementing sorting soon

--- adera/Topic.swift
+++ adera/Topic.swift
@@ -9,18 +9,21 @@ import FirebaseDatabase
 class Topic {
     let creatorUID: String
     let name: String
+    var creationDate: String?
 
-    init(creatorUID: String, name: String) {
+    init(creatorUID: String, name: String, creationDate: String) {
         self.creatorUID = creatorUID
         self.name = name
+        self.creationDate = creationDate
     }
 
     init(snapshot: FIRDataSnapshot) {
         self.name = snapshot.childSnapshot(forPath: "name").value as! String
         self.creatorUID = snapshot.childSnapshot(forPath: "creatorUID").value as! String
+        self.creationDate = snapshot.childSnapshot(forPath: "creationDate").value as? String
     }
 
     func toDictionary() -> Dictionary<String, Any> {
-        return ["name": name, "creatorUID": creatorUID]
+        return ["name": name, "creatorUID": creatorUID, "creationDate": creationDate!]
     }
 }
